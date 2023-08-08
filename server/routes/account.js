026eadd18e92b7904517d4e2ced51ad89903dca7
Pablo George Porras <93367833+PabloGPorras@users.noreply.github.com>
2023-08-08 22:13:25 +0000

    Pending changes exported from your codespace

--- server/routes/account.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const accountController = require('../controllers/accountController');
-
-router.get('/:userId', accountController.getUserAccount);
-router.put('/:userId', accountController.updateUserAccount);
-
-module.exports = router;
